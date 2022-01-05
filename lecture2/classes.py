7d56c2d128413dc0b781d2b1e9b975678699f5d6
guozekun <woxinguangmingguo@gmail.com>
2022-01-05 10:19:42 +0800

    Change classes.py

--- lecture2/classes.py
+++ lecture2/classes.py
@@ -1,8 +1,27 @@
-class Point():
-    def __init__(self, input1, input2):
-        self.x = input1
-        self.y = input2
+class Flight():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passengers = []
 
-p = Point(1, 2)
-print(p.x)
-print(p.y)
+    def add_passenger(self, name):
+        # if self.open_seats() == 0
+        # python not逻辑操作数字，not 0 为ture, not 2 为false
+        # not 对于空值为False, 实值为True
+        if not self.open_seats():
+            return False
+        else:    
+            self.passengers.append(name)
+        return True
+
+    def open_seats(self):
+        return self.capacity - len(self.passengers)    
+
+flight = Flight(3)
+people = ["Harry", "Ron", "Herminoe", "Ginny"]
+for person in people:
+    success = flight.add_passenger(person)
+    if success:
+        print(f"Add {person} to flight successfully")
+    else:
+        print(f"No avaliable seats for {person}")    
+
